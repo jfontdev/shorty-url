3f0a394c3d90c9f00828c22c822fe4a48f976ed3
Jordi <jordifv90@gmail.com>
2024-10-07 19:40:06 +0200

    refactor: used go fmt on the routes package

--- routes/routes.go
+++ routes/routes.go
@@ -11,18 +11,18 @@ import (
 // TODO: Change this to a POST method and use the body
 func RegisterRoutes(app *fiber.App) {
 	app.Get("/shorten/:url", func(c *fiber.Ctx) error {
-	params := c.Params("url")
-    log.Printf("URL to parse: %s", params)
+		params := c.Params("url")
+		log.Printf("URL to parse: %s", params)
 
-    hashed := sha256.Sum256([]byte(params))
-    log.Printf("Hashed: %x", hashed)
+		hashed := sha256.Sum256([]byte(params))
+		log.Printf("Hashed: %x", hashed)
 
-    encoded := base64.StdEncoding.EncodeToString(hashed[:])
-    log.Println("Encoded: ", encoded)
+		encoded := base64.StdEncoding.EncodeToString(hashed[:])
+		log.Println("Encoded: ", encoded)
 
-    shortenedUrl := encoded[:12] 
-    log.Println("Shortened URL: ", shortenedUrl)
+		shortenedUrl := encoded[:12]
+		log.Println("Shortened URL: ", shortenedUrl)
 
-    return c.JSON(fiber.Map{"shortened_url": shortenedUrl, "original_url": params})
+		return c.JSON(fiber.Map{"shortened_url": shortenedUrl, "original_url": params})
 	})
 }
