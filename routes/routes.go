42198f6a043ae77db9987b28f84f3b94601472b7
Jordi <jordifv90@gmail.com>
2024-10-07 19:37:09 +0200

    feat: First implementation of shorten with hashing and encoding

--- routes/routes.go
+++ routes/routes.go
@@ -1,17 +1,28 @@
 package routes
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+// TODO: Change this to a POST method and use the body
 func RegisterRoutes(app *fiber.App) {
 	app.Get("/shorten/:url", func(c *fiber.Ctx) error {
-		params := c.Params("url")
+	params := c.Params("url")
+    log.Printf("URL to parse: %s", params)
 
-		log.Print("URL to parse " + params)
+    hashed := sha256.Sum256([]byte(params))
+    log.Printf("Hashed: %x", hashed)
 
-		return c.JSON(params)
+    encoded := base64.StdEncoding.EncodeToString(hashed[:])
+    log.Println("Encoded: ", encoded)
+
+    shortenedUrl := encoded[:12] 
+    log.Println("Shortened URL: ", shortenedUrl)
+
+    return c.JSON(fiber.Map{"shortened_url": shortenedUrl, "original_url": params})
 	})
 }
