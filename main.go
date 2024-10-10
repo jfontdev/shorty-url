400946550c0343672652a019387020f4298c0852
Jordi <jordifv90@gmail.com>
2024-10-10 19:38:40 +0200

    feat: connect to db and use a general fiber logger on main

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"shorty-url/database"
 	"shorty-url/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func main() {
+	database.Connect()
+
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	routes.RegisterRoutes(app)
 
