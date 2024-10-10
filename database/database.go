b36555dd5737efd405155bba669bbbbf042f4832
Jordi <jordifv90@gmail.com>
2024-10-10 19:37:32 +0200

    feat: create db connection logging and automigration

---
+++ database/database.go
@@ -0,0 +1,51 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"shorty-url/config"
+	"shorty-url/models"
+	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var DB DbInstance
+
+// TODO: Remove automigrations.
+func Connect() {
+	p := config.Config("DB_PORT")
+
+	port, err := strconv.ParseUint(p, 10, 32)
+
+	if err != nil {
+		fmt.Println("Error parsing str to int")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+	 config.Config("DB_HOST"), config.Config("DB_USER"), config.Config("DB_PASSWORD"), config.Config("DB_NAME"), port)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Connected to the DB")
+
+	db.Logger = logger.Default.LogMode(logger.Warn)
+
+	db.AutoMigrate(&models.Url{})
+	
+	DB = DbInstance{Db: db}
+}
